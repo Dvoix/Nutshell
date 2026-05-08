794787fb26ade87a0bb52dd80ee2894360932d35
Dvoix <dvoix.dev@gmail.com>
2026-05-08 16:53:07 +0000

    stable version now on test not ready to main

--- src/nutshell/links/service.py
+++ src/nutshell/links/service.py
@@ -11,29 +11,31 @@ logger = logging.getLogger(__name__)
 
 class LinkService:
     def __init__(self, session: AsyncSession):
-        self.repo = LinkRepository(session)
         self.session = session
-    
-    async def create_short_link(session: AsyncSession, original_url: str, max_retries: int = 5):
+        self.repo = LinkRepository(session)
+
+    async def create_short_link(self, original_url: str, max_retries: int = 5):
         for attempt in range(max_retries):
             short_code = generate_short_code()
-        
-            short_link = LinkORM(
-            original_url=str(original_url),
-            short_code=short_code
-        )
-        
-            session.add(short_link)
-        
-            try:
-                await session.commit()
-                await session.refresh(short_link)
-                return short_link
-        
-            except IntegrityError:
-                await session.rollback()
-                logger.warning(
-                f"Collision detected for code: {short_code}. Retrying... (Attempt {attempt + 1})")
-            continue
 
-        raise RuntimeError(f"Could not generate a unique short link after {max_retries} attempts.")
+            short_link = LinkORM(
+                original_url=str(original_url),
+                short_code=short_code
+            )
+
+            self.session.add(short_link)
+
+            try:
+                await self.session.commit()
+                await self.session.refresh(short_link)
+                return short_link
+
+            except IntegrityError:
+                await self.session.rollback()
+                logger.warning(
+                    f"Collision detected for code: {short_code}. Retrying... (Attempt {attempt + 1})")
+                continue
+
+        raise RuntimeError(
+            f"Could not generate a unique short link after {max_retries} attempts."
+        )
--- src/nutshell/links/views.py
+++ src/nutshell/links/views.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from nutshell.database import db_helper
 
 from nutshell.links.models import LinkORM
-from nutshell.links.service import create_short_link
+from nutshell.links.service import LinkService
 from nutshell.links.schemas import URLCreate, URLOut
 
 
@@ -15,11 +15,14 @@ router = APIRouter()
 async def short_link_response(
   data: URLCreate, 
   session: AsyncSession = Depends(db_helper.session_getter)
-)-> dict[str, LinkORM]:
+):
+  service = LinkService(session)
+  short_code = await service.create_short_link(str(data.original_url))
   
-  short_code = await create_short_link(session, data.original_url)
   if not short_code:
+    
     raise HTTPException(
       status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
       detail="Could not generate unique short code try again")
+  
   return short_code
