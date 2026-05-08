cd792ab821800118cb01cd7cf56e474d5ee0fad4
Dvoix <dvoix.dev@gmail.com>
2026-05-08 10:55:20 +0000

    stable version ready to main

--- src/nutshell/links/models.py
+++ src/nutshell/links/models.py
@@ -1,10 +1,32 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, ForeignKey
 
 
 from nutshell.mixins.models import Base, CreatedAtMixin, IdPrimaryKeyMixin
+
+
+if TYPE_CHECKING:
+    from nutshell.users.models import User
+
+
 class Link(IdPrimaryKeyMixin, CreatedAtMixin, Base):
     __tablename__ = "links"
 
     original_url: Mapped[str] = mapped_column(nullable=False)
-    short_code: Mapped[str] = mapped_column(String(16), unique=True, nullable=False)
+
+    short_code: Mapped[str] = mapped_column(
+        String(16),
+        unique=True,
+        nullable=False
+    )
+
+    owner_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        nullable=False
+    )
+
+    owner: Mapped["User"] = relationship(
+        back_populates="links"
+    )
