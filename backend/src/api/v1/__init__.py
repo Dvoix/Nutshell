c8938878b3fe4e21b3e8f94c4e3636a838c3bd33
Dvoix <dvoix.dev@gmail.com>
2026-06-06 19:15:28 +0000

    small ruff fix

--- backend/src/api/v1/__init__.py
+++ backend/src/api/v1/__init__.py
@@ -1,27 +1,17 @@
 from fastapi import APIRouter
 
-from backend.src.config import settings
-
+from backend.src.api.v1.auth import router as auth_router
 from backend.src.api.v1.links import router as links_router
 from backend.src.api.v1.users import router as users_router
-from backend.src.api.v1.auth import router as auth_router
 
-router = APIRouter(
-  prefix=settings.api.v1.prefix
-)
-router.include_router(
-  links_router,
-  prefix=settings.api.v1.links
-)
+from backend.src.config import settings
+
+router = APIRouter(prefix=settings.api.v1.prefix)
+
+router.include_router(links_router, prefix=settings.api.v1.links)
 
 
-router.include_router(
-  users_router, 
-  prefix=settings.api.v1.users
-)
+router.include_router(users_router, prefix=settings.api.v1.users)
 
 
-router.include_router(
-  auth_router, 
-  prefix=settings.api.v1.auth
-)
+router.include_router(auth_router, prefix=settings.api.v1.auth)
