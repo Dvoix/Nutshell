5afda854ec7280fc0416e8aa7078a63b220cb6dd
Dvoix <dvoix.dev@gmail.com>
2026-06-06 19:05:22 +0000

    small ruff fix

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -16,25 +16,41 @@ if TYPE_CHECKING:
 
 
 class User(Base, IdPrimaryKeyMixin, CreatedAtMixin, UpdatedAtMixin):
-  __tablename__ = "users"
+    __tablename__ = "users"
 
-  username: Mapped[str] = mapped_column(String(30), unique=True, index=True, nullable=False)
-  
-  email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
-  
-  password_hash: Mapped[str] = mapped_column(String(255), unique=False, nullable=False)
-  
-  role: Mapped[UserRole] = mapped_column(
-    Enum(UserRole, name="userrole"), 
-    default=UserRole.user, 
-    nullable=False
-  )
-  
-  status: Mapped[UserStatus] = mapped_column(
-    Enum(UserStatus, name="userstatus"), 
-    default=UserStatus.active, 
-    nullable=False
-  )
+    username: Mapped[str] = mapped_column(
+        String(30),
+        unique=True,
+        index=True,
+        nullable=False,
+    )
 
-  links: Mapped[
-    list["Link"]] = relationship(back_populates="owner", cascade="all, delete-orphan")
+    email: Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        index=True,
+        nullable=False,
+    )
+
+    password_hash: Mapped[str] = mapped_column(
+        String(255),
+        unique=False,
+        nullable=False,
+    )
+
+    role: Mapped[UserRole] = mapped_column(
+        Enum(UserRole, name='userrole'),
+        default=UserRole.user,
+        nullable=False,
+    )
+
+    status: Mapped[UserStatus] = mapped_column(
+        Enum(UserStatus, name='userstatus'),
+        default=UserStatus.active,
+        nullable=False,
+    )
+
+    links: Mapped[list['Link']] = relationship(
+        back_populates='owner',
+        cascade='all, delete-orphan',
+    )
