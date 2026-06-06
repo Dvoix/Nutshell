49175d28efc146ed60717eec69b67228ec6decaa
Dvoix <dvoix.dev@gmail.com>
2026-06-06 18:33:47 +0000

    fixed create_user, aligned method signature, fixed get_admin_user

--- backend/src/users/repository.py
+++ backend/src/users/repository.py
@@ -5,8 +5,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.src.enums import UserRole
 from backend.src.users.models import User
-from backend.src.users.schemas import UserAuth
-
 
 logger = logging.getLogger(__name__)
 
@@ -15,40 +13,41 @@ class UserRepository:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    async def create_user(self, user: UserAuth, password_hash: str) -> User:
-        user = User(
-            username=user.username,
-            email=user.email,
-            password_hash=password_hash
+    async def create_user(
+        self,
+        username: str,
+        email: str,
+        password_hash: str,
+    ) -> User:
+        db_user = User(
+            username=username,
+            email=email,
+            password_hash=password_hash,
         )
-        self.session.add(user)
+        self.session.add(db_user)
         await self.session.flush()
-        return user
+        return db_user
 
     async def get_user_by_id(self, user_id: int) -> User | None:
         result = await self.session.execute(
-            select(User).where
-            (User.id == user_id)
+            select(User).where(User.id == user_id),
         )
         return result.scalar_one_or_none()
-    
+
     async def get_user_by_email(self, email: str) -> User | None:
         result = await self.session.execute(
-            select(User).where
-            (User.email == email)
+            select(User).where(User.email == email),
         )
         return result.scalar_one_or_none()
-    
+
     async def get_user_by_username(self, username: str) -> User | None:
         result = await self.session.execute(
-            select(User).where
-            (User.username == username)
+            select(User).where(User.username == username),
         )
         return result.scalar_one_or_none()
 
     async def get_admin_user(self) -> User | None:
         result = await self.session.execute(
-            select(User).where
-            (User.role == UserRole.admin)
+            select(User).where(User.role == UserRole.admin),
         )
-        return result
+        return result.scalar_one_or_none()
